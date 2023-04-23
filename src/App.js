8c7f870e227b3f8f1617bf041426c4a523c01195
AfzalUrRahman <afzalrahman.cse@gmail.com>
2023-04-23 06:02:39 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './Header'
+import Footer from './Footer'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Contact from './Contact'
+import Home from './Home'
+import About from './About'
+import React from 'react';
+import Todo from './Todo'
+import NotFound from './NotFound'
+import Signin from './Signin'
+import Signup from './Signup'
+const App = () => {
+    return <div>
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route path='/' element={<Home/>}/>
+                <Route path='/about' element={<About/>}/>    
+                <Route path='/todo' element={<Todo/>}/>    
+                <Route path='/contact' element={<Contact/>}/> 
+                <Route path='/signin' element={<Signin/>}/>
+                <Route path='/signup' element={<Signup/>}/>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+                <Route path='*' element={<NotFound/>}/>   
+
+            </Routes>
+
+
+            <hr />
+            <Footer />
+        </BrowserRouter>
     </div>
-  );
 }
 
 export default App;
